bc321641f5fdaa200cd56cb1bca245985eb4fd7c
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-04-29 04:22:41 +0700

    исправлена неточность в валидации signup

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.post("/signup", celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required().regex(PATTERN_URL),
+    avatar: Joi.string().regex(PATTERN_URL),
   }),
 }), createUser);
 
