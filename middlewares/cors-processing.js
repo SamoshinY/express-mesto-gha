18e043a875de6ca9bd904c438171f6e583a9cca6
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-11 05:23:52 +0700

    изменить настройки CORS

--- middlewares/cors-processing.js
+++ middlewares/cors-processing.js
@@ -12,6 +12,7 @@ module.exports = (req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", true);
   }
 
   const { method } = req;
@@ -22,6 +23,7 @@ module.exports = (req, res, next) => {
 
   if (method === "OPTIONS") {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
     res.header("Access-Control-Allow-Headers", requestHeaders);
     return res.end();
