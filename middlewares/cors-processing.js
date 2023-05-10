31f87f8669ce9ede34f2cb5de7e55e238d50a985
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-11 05:09:19 +0700

    изменить настройки CORS

--- middlewares/cors-processing.js
+++ middlewares/cors-processing.js
@@ -4,16 +4,14 @@ const allowedCors = [
   "http://samoshin.nomoredomains.monster",
   "https://samoshin.nomoredomains.monster",
   "localhost:3000",
+  "http://localhost:3000",
 ];
-
-// const xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", origin);
   }
 
   const { method } = req;
@@ -23,6 +21,7 @@ module.exports = (req, res, next) => {
   const requestHeaders = req.headers["access-control-request-headers"];
 
   if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
     res.header("Access-Control-Allow-Headers", requestHeaders);
     return res.end();
