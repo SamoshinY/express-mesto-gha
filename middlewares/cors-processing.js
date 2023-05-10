4f17a299502c27d51e2d1f210640bc9cbe95695d
SamoshinY <113671121+SamoshinY@users.noreply.github.com>
2023-05-11 03:35:41 +0700

    исправить CORS обработчик

--- middlewares/cors-processing.js
+++ middlewares/cors-processing.js
@@ -1,6 +1,8 @@
 const allowedCors = [
   "https://praktikum.tk",
   "http://praktikum.tk",
+  "http://samoshin.nomoredomains.monster",
+  "https://samoshin.nomoredomains.monster",
   "localhost:3000",
 ];
 
